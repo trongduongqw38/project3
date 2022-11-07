640f0bada99b2e63657ad501d6d9c6721e8db429
Dzuong <@trongduongqw37>
2022-11-07 23:56:24 +0700

    last commit

---
+++ deployment/deploy.sh
@@ -0,0 +1,23 @@
+kubectl apply -f aws-secret.yaml
+kubectl apply -f env-secret.yaml
+kubectl apply -f env-configmap.yaml
+
+kubectl apply -f backend-feed-deployment.yaml
+kubectl apply -f backend-feed-service.yaml
+
+kubectl apply -f backend-user-deployment.yaml
+kubectl apply -f backend-user-service.yaml
+
+kubectl apply -f reverseproxy-deployment.yaml
+kubectl apply -f reverseproxy-service.yaml
+
+
+kubectl expose deployment reverseproxy --type=LoadBalancer --name=publicreverseproxy
+
+
+
+kubectl apply -f frontend-deployment.yaml
+kubectl apply -f frontend-service.yaml
+kubectl expose deployment udagram-frontend --type=LoadBalancer --name=publicfrontend
+
+
